aec9167a3aeb82c79eb3dc8ed3df76575de6198e
Tor Hagland <torh@stacc.com>
2022-03-01 17:23:55 +0100

    remove echoes

--- action.sh
+++ action.sh
@@ -3,11 +3,6 @@ set -e
 
 echo Using CLI version: $(stacc --version)
 
-echo $INPUT_CLIENTID
-echo $INPUT_CLIENTSECRET
-echo $INPUT_MODULEDIR
-echo $INPUT_VERSION
-
 # Log in
 stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
