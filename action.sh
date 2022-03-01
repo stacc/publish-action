5df6d2fa922dcc86f56fdc217d64909ce6188889
Suneetha Kalakata <suneethak@stacc.com>
2022-03-01 19:51:34 +0100

    Reverted the changes

--- action.sh
+++ action.sh
@@ -4,7 +4,7 @@ set -e
 echo Using CLI version: $(stacc --version)
 
 # Log in
-stacc login -u $CLIENT_ID -p $CLIENT_SECRET
+stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
