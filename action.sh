f4417e3a4a6b487212b30985023ef2906f50aac1
Audun Stien <auduns@stacc.com>
2022-03-01 16:23:37 +0100

    remove unneseccary docker login

--- action.sh
+++ action.sh
@@ -8,8 +8,6 @@ CLIENT_ID=${clientId:?'clientId variable missing.'}
 CLIENT_SECRET=${clientSecret:?'clientSecret variable missing.'}
 VERSION=${version:?'version variable missing.'}
 
-echo $CLIENT_SECRET | docker login -u $CLIENT_ID --password-stdin stacc.azurecr.io
-
 # Log in
 stacc login -u $CLIENT_ID -p $CLIENT_SECRET
 
