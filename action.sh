0a7a497c38123d7d61d4c6516493e9875ec531ce
Audun Stien <auduns@stacc.com>
2022-03-01 14:54:21 +0100

    add inputs

--- action.sh
+++ action.sh
@@ -4,12 +4,12 @@ set -e
 echo Using CLI version: $(stacc --version)
 
 # Parameters
-CLIENT_ID=${CLIENT_ID:?'CLIENT_ID variable missing.'}
-CLIENT_SECRET=${CLIENT_SECRET:?'CLIENT_SECRET variable missing.'}
-VERSION=${VERSION:?'VERSION variable missing.'}
+CLIENT_ID=${clientId:?'clientId variable missing.'}
+CLIENT_SECRET=${clientSecret:?'clientSecret variable missing.'}
+VERSION=${version:?'version variable missing.'}
 
 # Log in
 stacc login -u $CLIENT_ID -p $CLIENT_SECRET
 
 # Publish artifacts
-stacc publish --version $VERSION .
+stacc publish --version $VERSION $moduleDir
