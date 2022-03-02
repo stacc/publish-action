45d19b50c436e0b1fb0975e861b067e0b6b48ee6
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 10:09:36 +0100

    Publish-action: Reverted all chenages

--- action.sh
+++ action.sh
@@ -1,17 +1,10 @@
-# #!usr/bin/bash bash
-# set -e
+#!usr/bin/bash bash
+set -e
 
-# echo Using CLI version: $(stacc --version)
+echo Using CLI version: $(stacc --version)
 
-# # Log in
-# stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
+# Log in
+stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
-# # Publish artifacts
-# stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
-
-
-#!/bin/sh -l
-
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# Publish artifacts
+stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
