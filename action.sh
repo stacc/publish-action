f64728fd1cc8a8b15cccbb537b21535583eb0c69
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 14:54:16 +0100

    some debugging stuff to view file permissions in actions

--- action.sh
+++ action.sh
@@ -5,7 +5,18 @@ echo Using CLI version: $(stacc --version)
 
 export STACC_CONFIG=$PWD/.staccrc
 # Log in
+
+export INPUT_CLIENTID=service-catalog-admin
+export INPUT_CLIENTSECRET=4d37fc99-5082-4dca-889f-a38b28145908
+export INPUT_VERSION=1.0.0
+export INPUT_MODULEDIR=./module
+
 stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
-chown -R 1001:1001 $STACC_CONFIG
+whoami
+echo "------------------"
+stat $STACC_CONFIG
+echo "------------------"
+ls -l $STACC_CONFIG
+
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
