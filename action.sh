52ec5957bc2ad0c6f737e39ba19c699cc19f4c8e
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 15:03:01 +0100

    move stacclogin

--- action.sh
+++ action.sh
@@ -5,13 +5,12 @@ echo Using CLI version: $(stacc --version)
 
 export STACC_CONFIG=$PWD/.staccrc
 # Log in
-echo "does this work?" > $STACC_CONFIG
 whoami
+stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 echo "------------------"
 stat $STACC_CONFIG
 echo "------------------"
 ls -l $STACC_CONFIG
 
-stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
