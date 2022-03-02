6901af72ea8227e7ae54ddf3619a022d78c19527
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 15:04:04 +0100

    remove debug stuff from action.sh

--- action.sh
+++ action.sh
@@ -5,12 +5,7 @@ echo Using CLI version: $(stacc --version)
 
 export STACC_CONFIG=$PWD/.staccrc
 # Log in
-whoami
 stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
-echo "------------------"
-stat $STACC_CONFIG
-echo "------------------"
-ls -l $STACC_CONFIG
 
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
