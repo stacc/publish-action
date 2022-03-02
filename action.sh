41b2adb4992dc28e6167e005ada06929170fcf16
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 14:57:33 +0100

    it seems the problem was logging in maybe

--- action.sh
+++ action.sh
@@ -5,18 +5,14 @@ echo Using CLI version: $(stacc --version)
 
 export STACC_CONFIG=$PWD/.staccrc
 # Log in
+touch $STACC_CONFIG
 
-export INPUT_CLIENTID=service-catalog-admin
-export INPUT_CLIENTSECRET=4d37fc99-5082-4dca-889f-a38b28145908
-export INPUT_VERSION=1.0.0
-export INPUT_MODULEDIR=./module
-
-stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 whoami
 echo "------------------"
 stat $STACC_CONFIG
 echo "------------------"
 ls -l $STACC_CONFIG
 
+stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
