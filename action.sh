005aa1ad1d68b01d1cd36ea8b6d53087bf0b6ae0
Suneetha Kalakata <suneethak@stacc.com>
2022-03-02 14:35:56 +0100

    add chown

--- action.sh
+++ action.sh
@@ -7,7 +7,7 @@ export STACC_CONFIG=$PWD/.staccrc
 # Log in
 stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
-chmod 444 $STACC_CONFIG
+chown 1001:1001 $STACC_CONFIG
 
 # Publish artifacts
 sudo stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
