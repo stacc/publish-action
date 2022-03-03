3b63f84ea991543ac9ebfaa3225be6d21a7f1b4d
Suneetha Kalakata <suneethak@stacc.com>
2022-03-03 14:17:28 +0100

    more echos

--- action.sh
+++ action.sh
@@ -9,5 +9,8 @@ stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 echo "module absolute path" $PWD
 echo "module path"  $INPUT_MODULEDIR
 
+echo "------------------"
+ls -l $INPUT_MODULEDIR
+
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
