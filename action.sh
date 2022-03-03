cf0557c5aa267b5dc4716fc9f4dbfdbd59114dc8
Suneetha Kalakata <suneethak@stacc.com>
2022-03-03 12:16:33 +0100

    action: debbuging

--- action.sh
+++ action.sh
@@ -6,5 +6,7 @@ echo Using CLI version: $(stacc --version)
 # Log in
 stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
+echo "module path" $INPUT_MODULEDIR
+
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
