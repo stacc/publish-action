51966b744c35d472aeae56258922b3126f16328c
Suneetha Kalakata <suneethak@stacc.com>
2022-03-03 12:46:21 +0100

    Removed CD

--- action.sh
+++ action.sh
@@ -8,6 +8,6 @@ stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
 
 echo "module absolute path" $PWD
 echo "module path"  $INPUT_MODULEDIR
-cd $INPUT_MODULEDIR
+
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
