af0660b3e5163eee51eb671da45334775ef7801c
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2022-03-04 12:59:58 +0100

    Validate inputs in script

--- action.sh
+++ action.sh
@@ -4,8 +4,13 @@ set -e
 
 echo Using CLI version: $(stacc --version)
 
+# Inputs
+CLIENT_ID=${INPUT_CLIENTID:?'Client ID input missing.'}
+CLIENT_SECRET=${INPUT_CLIENTSECRET:?'Client secret input missing.'}
+VERSION=${INPUT_VERSION:?'Version input missing.'}
+
 # Log in
-stacc login -u "$INPUT_CLIENTID" -p "$INPUT_CLIENTSECRET"
+stacc login -u "$CLIENT_ID" -p "$CLIENT_SECRET"
 
 # Publish artifacts
-stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
+stacc publish --version $VERSION $INPUT_MODULEDIR
