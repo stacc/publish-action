f5852bf5f660faa4851dd603e8b389b832234ad4
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2022-03-04 12:19:31 +0100

    Add quotes

--- action.sh
+++ action.sh
@@ -5,7 +5,7 @@ set -e
 echo Using CLI version: $(stacc --version)
 
 # Log in
-stacc login -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET
+stacc login -u "$INPUT_CLIENTID" -p "$INPUT_CLIENTSECRET"
 
 # Publish artifacts
 stacc publish --version $INPUT_VERSION $INPUT_MODULEDIR
